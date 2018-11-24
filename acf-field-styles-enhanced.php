eadaf23ee7f6da57e723c7e9c91236ce0f01bfbc
Matthew McVickar <matthew@matthewmcvickar.com>
2018-11-23 19:53:13 -0800

    Fix missing lines between nested repeater rows.

--- acf-field-styles-enhanced.php
+++ acf-field-styles-enhanced.php
@@ -4,7 +4,7 @@
  * Plugin Name:       ACF Field Styles Enhanced
  * Plugin URI:        https://github.com/matthewmcvickar/acf-field-styles-enhanced
  * Description:       A handful of styling improvements and CSS classes for ACF field groups to make for prettier editing.
- * Version:           0.0.7
+ * Version:           0.0.8
  * Author:            Matthew McVickar
  * Author URI:        http://matthewmcvickar.com
  * License:           GPL-2.0+
@@ -13,9 +13,11 @@
  * GitHub Plugin URI: https://github.com/matthewmcvickar/acf-field-styles-enhanced
  */
 
+define( 'acf_field_styles_enhanced_plugin_version', '0.0.8' );
+
 add_action( 'admin_enqueue_scripts', 'enqueue_acf_field_styles_enhanced__load_stylesheet', PHP_INT_MAX );
 function enqueue_acf_field_styles_enhanced__load_stylesheet() {
-  wp_enqueue_style( 'acf-field-styles-enhanced', plugin_dir_url( __FILE__ ) . 'acf-field-styles-enhanced.css', null );
+  wp_enqueue_style( 'acf-field-styles-enhanced', plugin_dir_url( __FILE__ ) . 'acf-field-styles-enhanced.css', array(), ACF_FIELD_STYLES_ENHANCED_PLUGIN_VERSION );
 }
 
 ?>
