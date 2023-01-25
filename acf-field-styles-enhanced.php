b420bc1f8976271af8e4f6907b3dbc76d5b18be7
Matthew McVickar <matthew@matthewmcvickar.com>
2023-01-25 09:22:29 -0800

    version bump

--- acf-field-styles-enhanced.php
+++ acf-field-styles-enhanced.php
@@ -4,16 +4,16 @@
  * Plugin Name:       ACF Field Styles Enhanced
  * Plugin URI:        https://github.com/matthewmcvickar/acf-field-styles-enhanced
  * Description:       A handful of styling improvements and CSS classes for ACF field groups to make for prettier editing.
- * Version:           0.0.32
+ * Version:           0.0.33
  * Author:            Matthew McVickar
- * Author URI:        http://matthewmcvickar.com
+ * Author URI:        https://matthewmcvickar.com
  * License:           MIT
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       acf-field-styles-enhanced
  * GitHub Plugin URI: https://github.com/matthewmcvickar/acf-field-styles-enhanced
  */
 
-define( 'ACF_FIELD_STYLES_ENHANCED_PLUGIN_VERSION', '0.0.32' );
+define( 'ACF_FIELD_STYLES_ENHANCED_PLUGIN_VERSION', '0.0.33' );
 
 add_action( 'admin_enqueue_scripts', 'enqueue_acf_field_styles_enhanced__load_stylesheet', PHP_INT_MAX );
 function enqueue_acf_field_styles_enhanced__load_stylesheet() {
